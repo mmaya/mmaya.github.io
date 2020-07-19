8814e1cb8ba6a48c0819d2983527b46bcaf5c5a1
Milleni Maya <milleni.maya@gmail.com>
2020-07-19 10:56:47 -0300

    Updates

---
+++ precache-manifest.0bcfd9ad21b0ebeb8a1af6b7d6b36d92.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e1eb3b4eac152169942262f850b45f",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2730bb62b13ba247ea42",
+    "url": "/my-portfolio/static/css/2.4bfc5d82.chunk.css"
+  },
+  {
+    "revision": "2730bb62b13ba247ea42",
+    "url": "/my-portfolio/static/js/2.9eb6e22b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-portfolio/static/js/2.9eb6e22b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bebf558dc8f7440b3a58",
+    "url": "/my-portfolio/static/js/main.e978c7e9.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "789374a0fd47d3fb15483177407bc75f",
+    "url": "/my-portfolio/static/media/advent-pro-latin-100.789374a0.woff"
+  },
+  {
+    "revision": "eb23e16d47d6cf8a12913fcc547e2488",
+    "url": "/my-portfolio/static/media/advent-pro-latin-100.eb23e16d.woff2"
+  },
+  {
+    "revision": "1c1766d01c8a63a3e9dcfbefbd629a7b",
+    "url": "/my-portfolio/static/media/advent-pro-latin-200.1c1766d0.woff"
+  },
+  {
+    "revision": "9909c0ed8189a2a6b33abd433f90b9de",
+    "url": "/my-portfolio/static/media/advent-pro-latin-200.9909c0ed.woff2"
+  },
+  {
+    "revision": "3651854f2493f3347de04985b504544b",
+    "url": "/my-portfolio/static/media/advent-pro-latin-300.3651854f.woff2"
+  },
+  {
+    "revision": "53fef75f13edd46cc158ac2c951c2035",
+    "url": "/my-portfolio/static/media/advent-pro-latin-300.53fef75f.woff"
+  },
+  {
+    "revision": "094d8523a225faee9a2c5e7d290d0f48",
+    "url": "/my-portfolio/static/media/advent-pro-latin-400.094d8523.woff"
+  },
+  {
+    "revision": "30b71a1e2036a397b136035da3a45af9",
+    "url": "/my-portfolio/static/media/advent-pro-latin-400.30b71a1e.woff2"
+  },
+  {
+    "revision": "c28c899324d8a2459db1754eb813780e",
+    "url": "/my-portfolio/static/media/advent-pro-latin-500.c28c8993.woff"
+  },
+  {
+    "revision": "eaf653c6ed6f137874ab5da2d7ee1239",
+    "url": "/my-portfolio/static/media/advent-pro-latin-500.eaf653c6.woff2"
+  },
+  {
+    "revision": "376285cac10e5157a0d2c2f027c6ef6e",
+    "url": "/my-portfolio/static/media/advent-pro-latin-600.376285ca.woff"
+  },
+  {
+    "revision": "a8d321a14778c0cf9b399691e8401b00",
+    "url": "/my-portfolio/static/media/advent-pro-latin-600.a8d321a1.woff2"
+  },
+  {
+    "revision": "c1a1838037c662e43bafa8adea268f1d",
+    "url": "/my-portfolio/static/media/advent-pro-latin-700.c1a18380.woff2"
+  },
+  {
+    "revision": "f70984e59249e07ad67cd224009b09d3",
+    "url": "/my-portfolio/static/media/advent-pro-latin-700.f70984e5.woff"
+  },
+  {
+    "revision": "33802f1e7390b174f0cdff33c6e00009",
+    "url": "/my-portfolio/static/media/perfil.33802f1e.png"
+  },
+  {
+    "revision": "45ad0dd64d5e155b031ead6bd84e3415",
+    "url": "/my-portfolio/static/media/portfolio.45ad0dd6.png"
+  },
+  {
+    "revision": "16a73d944d9825af5a5c1a1f55de8f28",
+    "url": "/my-portfolio/static/media/ubuntu-latin-300.16a73d94.woff2"
+  },
+  {
+    "revision": "f47318810718978402787f6c97d80b3c",
+    "url": "/my-portfolio/static/media/ubuntu-latin-300.f4731881.woff"
+  },
+  {
+    "revision": "094f326bbf5d701e828be07645618e13",
+    "url": "/my-portfolio/static/media/ubuntu-latin-300italic.094f326b.woff2"
+  },
+  {
+    "revision": "1f11b1e747c45516b1a5478480d6cedd",
+    "url": "/my-portfolio/static/media/ubuntu-latin-300italic.1f11b1e7.woff"
+  },
+  {
+    "revision": "1e926e228a9e2e1e77034f624211e2b4",
+    "url": "/my-portfolio/static/media/ubuntu-latin-400.1e926e22.woff"
+  },
+  {
+    "revision": "5b23eeb3a32b30e91682d601535d2a89",
+    "url": "/my-portfolio/static/media/ubuntu-latin-400.5b23eeb3.woff2"
+  },
+  {
+    "revision": "27149a968393dbdc0d872f05f177e843",
+    "url": "/my-portfolio/static/media/ubuntu-latin-400italic.27149a96.woff"
+  },
+  {
+    "revision": "ff2b5b4da106e880a3f3733fd5d77ceb",
+    "url": "/my-portfolio/static/media/ubuntu-latin-400italic.ff2b5b4d.woff2"
+  },
+  {
+    "revision": "b4e565dcfc8f6cb332be0fc03302ad99",
+    "url": "/my-portfolio/static/media/ubuntu-latin-500.b4e565dc.woff2"
+  },
+  {
+    "revision": "e5613b0c875443174f948840c8ce07f6",
+    "url": "/my-portfolio/static/media/ubuntu-latin-500.e5613b0c.woff"
+  },
+  {
+    "revision": "657fed7d867932571e9fa5fab260ed90",
+    "url": "/my-portfolio/static/media/ubuntu-latin-500italic.657fed7d.woff2"
+  },
+  {
+    "revision": "f068ac523f3d278ff11cfa4010af4d2a",
+    "url": "/my-portfolio/static/media/ubuntu-latin-500italic.f068ac52.woff"
+  },
+  {
+    "revision": "669545b14b8032954bfe46909785c19f",
+    "url": "/my-portfolio/static/media/ubuntu-latin-700.669545b1.woff"
+  },
+  {
+    "revision": "b91fae466c698c775adb2ae92cecc8b2",
+    "url": "/my-portfolio/static/media/ubuntu-latin-700.b91fae46.woff2"
+  },
+  {
+    "revision": "ad7e4c2f2d357a1eb39a11ecc1ad43aa",
+    "url": "/my-portfolio/static/media/ubuntu-latin-700italic.ad7e4c2f.woff"
+  },
+  {
+    "revision": "c02fdf8544ee71a10050b800f1c146b1",
+    "url": "/my-portfolio/static/media/ubuntu-latin-700italic.c02fdf85.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0568db0c197b0cf65afe682510c0d85.js"
+  "/my-portfolio/precache-manifest.0bcfd9ad21b0ebeb8a1af6b7d6b36d92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
